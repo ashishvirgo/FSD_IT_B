647d6b54cc7a5f3d4acc3831a79fcb140c698454
Ashish Bajpai <ashish.bajpai@abes.ac.in>
2025-04-21 12:20:09 +0530

    user app created

--- userapp/Server/model/usermodel.js
+++ userapp/Server/model/usermodel.js
@@ -3,8 +3,8 @@ const userSchema=new mongoose.Schema({
     name:{type: String,required:true},
     email:{type: String,required:true,unique:true},
     password:{type:String,required:true},
-    role:{type:String,required:true}
-});
+    role:{type:String,enum:['admin','student','teacher'],required:true}
+},{timestamps:true});
 
 const User=mongoose.model("users",userSchema);
 module.exports=User;
